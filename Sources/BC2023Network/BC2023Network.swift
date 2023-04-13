0d0c30d5ab1381d443781842d433f544482b1be1
jacostaf10 <jorge.acosta.freire@gmail.com>
2023-04-13 21:08:18 +0200

    v0.5

--- Sources/BC2023Network/BC2023Network.swift
+++ Sources/BC2023Network/BC2023Network.swift
@@ -4,12 +4,12 @@ public final class BCNetwork {
     
     public static let shared = BCNetwork()
     
-    public func getJSON<JSON: Codable>(request: URLRequest, type: JSON.Type) async throws -> JSON {
+    public func getJSON<JSON: Codable>(request: URLRequest, type: JSON.Type, decoder: JSONDecoder = JSONDecoder()) async throws -> JSON {
         let (data, response) = try await URLSession.shared.dataRequest(for: request)
         guard let res = response as? HTTPURLResponse else {throw NetworkError.noHTTP}
         if res.statusCode == 200 {
             do {
-                return try JSONDecoder().decode(JSON.self, from: data)
+                return try decoder.decode(JSON.self, from: data)
             } catch {
                 throw NetworkError.json(error)
             }
@@ -18,7 +18,7 @@ public final class BCNetwork {
         }
     }
     
-    public func postJSON<JSON: Codable> (request: URLRequest, json: JSON, statusOK: Int = 200) async throws {
+    public func postJSON<JSON: Codable> (request: URLRequest, json: JSON, statusOK: Int = 200, decoder: JSONDecoder = JSONDecoder()) async throws {
         let (_, response) = try await URLSession.shared.dataRequest(for: request)
         guard let response = response as? HTTPURLResponse else { throw NetworkError.noHTTP}
         if response.statusCode != statusOK {
